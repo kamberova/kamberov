c444cd702ba7045f75bc4e14ba0b51d6d6b55cb6
kamberova <80117288+kamberova@users.noreply.github.com>
2022-10-10 16:33:00 +0200

    adding firebase-configuration

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+const firebaseConfig = {
+  apiKey: process.env.Kambera_api,
+  authDomain: "kambera-28897.firebaseapp.com",
+  databaseURL: "https://kambera-28897-default-rtdb.firebaseio.com",
+  projectId: "kambera-28897",
+  storageBucket: "kambera-28897.appspot.com",
+  messagingSenderId: "21370708893",
+  appId: "1:21370708893:web:348d98c2199b72a8378574"
+};
+
+
+const app = initializeApp(firebaseConfig);
