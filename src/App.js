576a50e790bea7362ff43324784263cdffc68aa5
kamberova <80117288+kamberova@users.noreply.github.com>
2022-10-13 17:04:03 +0200

    accessing the firebase database

--- src/App.js
+++ src/App.js
@@ -1,33 +1,50 @@
 
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import { db } from './firebase-config';
-import { collection, getDoc } from 'firebase/firestore';
+import db from './firebase-config.js';
+import { collection, getDocs } from 'firebase/firestore/lite';
 
 
 
 function App() {
 
   const [users, setUsers] = useState([]);
-  const usersCollectionRef = collection(db, "users");
+  const usersCollectionRef = collection(db, 'users');
 
   useEffect(() => {
-    
+
     const getUsers = async () => {
-
-      const data = await getDoc(usersCollectionRef);
-      console.log(data)
-
-
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+      console.log(data);
     }
 
+    // async function getUsers() {
+    //   const usersCollectionRef = collection(db, 'users');
+    //   const data = await getDocs(usersCollectionRef);
+
+    //   console.log(data)
+
+    //   return data;
+    // }
+
     getUsers();
-  }, [])
+
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-       <p>Hello there</p>
-       <p>Are we there yet?</p>
+        <p>Hello there</p>
+       {users.map((user) => {
+        return (
+          <div>
+            {" "}
+            <h1>Email: {user.email}</h1>
+            <h3>Password: {user.password}</h3>
+          </div>
+        )
+       })}
       </header>
     </div>
   );
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,16 +1,18 @@
 
+import * as firebase from "firebase/app";
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "@firebase/firestore";
+import "firebase/database";
+import { getFirestore } from 'firebase/firestore/lite';
 
-require('dotenv').config();
+// require("firebase/database");
+// require('dotenv').config();
 
-console.log(process.env_sample.API_KEY);
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 
 const firebaseConfig = {
-  apiKey: process.env_sample.API_KEY,
+  apiKey: process.env.API_KEY,
   authDomain: "kambera-28897.firebaseapp.com",
   databaseURL: "https://kambera-28897-default-rtdb.firebaseio.com",
   projectId: "kambera-28897",
@@ -19,7 +21,8 @@ const firebaseConfig = {
   appId: "1:21370708893:web:348d98c2199b72a8378574"
 };
 
+const app= initializeApp(firebaseConfig);
 
-const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-export const db = getFirestore(app);
+export default db;
