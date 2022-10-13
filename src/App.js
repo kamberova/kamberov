84b1fb7d08ba3c5fa359295adadfcc39fb5b8661
kamberova <80117288+kamberova@users.noreply.github.com>
2022-10-13 18:17:28 +0200

    rendering the users data

--- src/App.js
+++ src/App.js
@@ -15,18 +15,10 @@ function App() {
 
     const getUsers = async () => {
       const data = await getDocs(usersCollectionRef);
-      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
-      console.log(data);
-    }
+      setUsers(data.docs.map((user) => ({ ...user.data(), id: user.id })))
+     
+    };
 
-    // async function getUsers() {
-    //   const usersCollectionRef = collection(db, 'users');
-    //   const data = await getDocs(usersCollectionRef);
-
-    //   console.log(data)
-
-    //   return data;
-    // }
 
     getUsers();
 
@@ -36,15 +28,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Hello there</p>
-       {users.map((user) => {
-        return (
-          <div>
-            {" "}
-            <h1>Email: {user.email}</h1>
-            <h3>Password: {user.password}</h3>
-          </div>
-        )
-       })}
+        {users.map((user) => {
+          return (
+            <ul>
+              <li key={user.id}>
+                <p>Email: {user.email}</p>
+                <p>Password: {user.password}</p>
+              </li>
+            </ul>
+          )
+        })}
       </header>
     </div>
   );
