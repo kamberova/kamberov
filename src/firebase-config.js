c89e0039161fa0cb9ad76899d807b768621bc368
kamberova <80117288+kamberova@users.noreply.github.com>
2022-10-19 16:53:41 +0200

    env configuration

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,28 +1,41 @@
 
-// import * as firebase from "firebase/app";
-import { initializeApp } from "firebase/app";
-import "firebase/database";
-import { getFirestore } from 'firebase/firestore/lite';
-
-// require("firebase/database");
-// require('dotenv').config();
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import * as firebase from "firebase/app";
+import "firebase/auth";
 
 
-const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: "kambera-28897.firebaseapp.com",
-  databaseURL: "https://kambera-28897-default-rtdb.firebaseio.com",
-  projectId: "kambera-28897",
-  storageBucket: "kambera-28897.appspot.com",
-  messagingSenderId: "21370708893",
-  appId: "1:21370708893:web:348d98c2199b72a8378574"
-};
-
-const app= initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
+const db = firebase.initializeApp ({
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_API_ID
+});
 
 export default db;
+
+
+
+
+// // import * as firebase from "firebase/app";
+// import { initializeApp } from "firebase/app";
+// import "firebase/database";
+// import { getFirestore } from 'firebase/firestore/lite';
+
+
+// const firebaseConfig = {
+//   apiKey: process.env.API_KEY,
+//   authDomain: process.env.API_DOMAIN,
+//   databaseURL: process.env.API_DATABASEURL,
+//   projectId: process.env.API_PROJECTID,
+//   storageBucket: process.env.API_STORAGEBUCKET,
+//   messagingSenderId: process.env.API_MESSAGINGSENDERID,
+//   appId: process.env.API_APIID
+// };
+
+// const app= initializeApp(firebaseConfig);
+
+// const db = getFirestore(app);
+
+// export default db;
