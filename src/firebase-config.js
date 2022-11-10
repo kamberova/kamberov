1a8dd4585f714c6d05fb40020acceace4b0110ef
kamberova <80117288+kamberova@users.noreply.github.com>
2022-11-10 13:59:27 +0100

    ENV

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -25,20 +25,20 @@ import 'firebase/storage';
 
 const firebaseConfig = {
 
-  // apiKey: "AIzaSyBD6bIbamynvjsNbDJJ1zY4nmBP5JFO5Ms",
-  // authDomain: "kambera-685c9.firebaseapp.com",
-  // projectId: "kambera-685c9",
-  // storageBucket: "kambera-685c9.appspot.com",
-  // messagingSenderId: "759438117224",
-  // appId: "1:759438117224:web:76844995fb1e3c24352edd",
-  // measurementId: "G-NGW2JR0CL9"
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyBD6bIbamynvjsNbDJJ1zY4nmBP5JFO5Ms",
+  authDomain: "kambera-685c9.firebaseapp.com",
+  projectId: "kambera-685c9",
+  storageBucket: "kambera-685c9.appspot.com",
+  messagingSenderId: "759438117224",
+  appId: "1:759438117224:web:76844995fb1e3c24352edd",
+  measurementId: "G-NGW2JR0CL9"
+  // apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  // // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
