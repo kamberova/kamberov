83434afba3a388a6f1e508156b4db408e7e9421e
kamberova <80117288+kamberova@users.noreply.github.com>
2022-11-10 14:50:34 +0100

    home

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -21,7 +21,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import 'firebase/storage';
-
+import { getStorage } from 'firebase/storage'
+// import { getFirestore } from 'firebase/firestore/lite'
 
 const firebaseConfig = {
 
@@ -43,5 +44,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
 
 export default app;
